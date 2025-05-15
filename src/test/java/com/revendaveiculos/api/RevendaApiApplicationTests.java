98851520961ef84c3b7666d6222ed73e742ab2c2
angelazgiampaoli <azgiampaoli@gmail.com>
2025-05-15 18:13:07 -0300

    AJUSTE TESTE

--- src/test/java/com/revendaveiculos/api/RevendaApiApplicationTests.java
+++ src/test/java/com/revendaveiculos/api/RevendaApiApplicationTests.java
@@ -2,9 +2,11 @@ package com.revendaveiculos.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RevendaApiApplicationTests {
 
 	@Test
