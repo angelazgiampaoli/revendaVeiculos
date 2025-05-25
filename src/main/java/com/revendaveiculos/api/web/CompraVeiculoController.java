da7c76bb5d8778088278dacef383841102962709
angelazgiampaoli <azgiampaoli@gmail.com>
2025-05-25 12:08:36 -0300

    readme e security

---
+++ src/main/java/com/revendaveiculos/api/web/CompraVeiculoController.java
@@ -0,0 +1,28 @@
+package com.revendaveiculos.api.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revendaveiculos.api.application.CadastroVeiculoService;
+import com.revendaveiculos.api.domain.model.Veiculo;
+
+@RestController
+@RequestMapping("/veiculos")
+public class CompraVeiculoController {
+
+    @Autowired
+    private CadastroVeiculoService cadastroVeiculoService;
+
+    @PostMapping("/comprar/{id}")
+    public ResponseEntity<String> comprar(@PathVariable Long id) {
+        Veiculo veiculo = cadastroVeiculoService.editar(id, new Veiculo());
+        veiculo.setVendido(true);
+        cadastroVeiculoService.cadastrar(veiculo);
+        return ResponseEntity.ok("Compra realizada com sucesso!");
+    }
+}
+
